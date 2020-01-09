3730fb2086cc9f7fe8da1184c7398da89c14d144
cg99 <darkinfinitespace@gmail.com>
2020-01-09 04:38:07 +0000

    remove storing in array process

--- pe1.cpp
+++ pe1.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int sumOf3and5() {
 	int a;
 	int sum = 0;
-	vector<int> arr;
 
 	cout << "The multiple of 3 or 5 below 1000 are: ";
 
   	for (a = 0; a < 1000; a++) {
     	if (a % 3 == 0 || a % 5 == 0 ) {
 			cout << a << endl;
-			arr.push_back(a);
+			sum += a;
     	}
   	}
-	 
-	for (int i = 0; i < arr.size(); i++){
-		sum = sum + arr[i];
-	}
 	
   	cout << "Sum of all the multiples of 3 or 5 below 1000 is \n" << sum;
 
